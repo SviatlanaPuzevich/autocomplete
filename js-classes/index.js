98e154ac985ee7c5a41078fe7f3aeb031ef48bce
puzevich <puzevich.svetlana@gmail.com>
2026-06-03 10:11:09 +0300

    feat: implement IntBuilder

--- js-classes/index.js
+++ js-classes/index.js
@@ -5,12 +5,42 @@ class BaseBuilder {
 
   plus(...values) {
     this.value = values.reduce((acc, cur) => acc + cur, this.value);
+    return this;
+  }
+
+  get() {
     return this.value;
   }
 }
 
-class IntBuilder {
+class IntBuilder extends BaseBuilder {
+  constructor(value) {
+    super(value);
+  }
 
+  minus(...values) {
+    this.value = values.reduce((acc, cur) => acc - cur, this.value);
+    return this;
+  }
+
+  multiply(n) {
+    this.value *= n;
+    return this;
+  }
+
+  divide(n) {
+    this.value /= n;
+    return this;
+  }
+
+  mod(n) {
+    this.value %= n;
+    return this;
+  }
+
+  static random(from, to) {
+    return Math.floor(Math.random() * (from - to + 1) + to);
+  }
 }
 
 class StringBuilder {
