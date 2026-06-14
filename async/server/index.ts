778a8fd7bd1d4463dca8614ebb8c5a2f2cbca213
puzevich <puzevich.svetlana@gmail.com>
2026-06-15 00:00:38 +0300

    feat: resource pool

--- async/server/index.ts
+++ async/server/index.ts
@@ -1,7 +1,68 @@
 import {DataItem, Executor} from "./executor";
 
-const task = ([]: DataItem []): Promise<void>=>{
-    return new Promise(resolve => {})
+const URL = 'http://localhost:3000/send';
+const MAX_CONCURRENT_REQUESTS = 20;
+const counter = {
+    success: 0,
+    failed: 0,
+    pending: 0,
+    lost: 0
+}
+
+const task = async (data: DataItem []): Promise<void> => {
+    const workers: Promise<void>[] = [];
+    for (let index = 0; index < MAX_CONCURRENT_REQUESTS; index++) {
+        workers.push(worker(data));
+    }
+    await Promise.all(workers);
+
+    console.log("--- Final Results ---", counter);
+
+}
+
+const worker = async (data: DataItem []): Promise<void> => {
+    while (data.length > 0) {
+        const item = data.pop()!;
+        counter.pending++;
+        try {
+            await sendRequest(item);
+            counter.success++;
+        } catch (error) {
+
+            if (error instanceof Error) {
+                if (error.message === 'Server Overload') {
+                    counter.lost++;
+                } else {
+                    counter.failed++;
+                }
+            }
+
+        } finally {
+            counter.pending--;
+        }
+    }
+}
+
+const sendRequest = async (item: DataItem): Promise<void> => {
+    const response = await fetch(URL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(item)
+    });
+
+    const result = await response.json();
+    console.log('Successes :', result);
+
+    if (!response.ok) {
+
+        const errorMessage = (result && typeof result === 'object' && 'error' in result)
+            ? String(result.error)
+            : `Server responded with status ${response.status}`;
+        throw new Error(errorMessage);
+    }
+
 }
 
 const executor = new Executor(task);
