1450a10483ee51883e12de22d87574acc91a97dc
puzevich <puzevich.svetlana@gmail.com>
2026-06-17 22:06:10 +0300

    feat: simple test

--- basic-testing/src/01-simple-tests/index.test.ts
+++ basic-testing/src/01-simple-tests/index.test.ts
@@ -1,32 +1,45 @@
-// Uncomment the code below and write your tests
-// import { simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
 describe('simpleCalculator tests', () => {
   test('should add two numbers', () => {
-    // Write your test here
+    expect(simpleCalculator({ a: 3, b: 5, action: Action.Add })).toBe(8);
   });
 
   test('should subtract two numbers', () => {
-    // Write your test here
+    expect(simpleCalculator({ a: 3, b: 5, action: Action.Subtract })).toBe(-2);
   });
 
   test('should multiply two numbers', () => {
-    // Write your test here
+    expect(simpleCalculator({ a: 3, b: 5, action: Action.Multiply })).toBe(15);
   });
 
   test('should divide two numbers', () => {
-    // Write your test here
+    expect(simpleCalculator({ a: 3, b: 5, action: Action.Divide })).toBe(0.6);
+    expect(simpleCalculator({ a: 3, b: 0, action: Action.Divide })).toBe(
+      Infinity,
+    );
   });
 
   test('should exponentiate two numbers', () => {
-    // Write your test here
+    expect(simpleCalculator({ a: 3, b: 5, action: Action.Exponentiate })).toBe(
+      243,
+    );
   });
 
   test('should return null for invalid action', () => {
-    // Write your test here
+    expect(simpleCalculator({ a: 3, b: 5, action: '**' })).toBeNull();
   });
 
   test('should return null for invalid arguments', () => {
-    // Write your test here
+    expect(
+      simpleCalculator({ a: 3, b: 's', action: Action.Subtract }),
+    ).toBeNull();
+    expect(simpleCalculator({ a: 'm', b: 5, action: Action.Add })).toBeNull();
+    expect(
+      simpleCalculator({ a: null, b: 5, action: Action.Subtract }),
+    ).toBeNull();
+    expect(
+      simpleCalculator({ a: 3, b: undefined, action: Action.Multiply }),
+    ).toBeNull();
   });
 });
