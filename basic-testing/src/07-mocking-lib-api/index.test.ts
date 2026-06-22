fe9757f55ed1a42092f25a57ed88604ff0e74dc0
puzevich <puzevich.svetlana@gmail.com>
2026-06-22 14:52:05 +0300

    feat: lib api mocking

--- basic-testing/src/07-mocking-lib-api/index.test.ts
+++ basic-testing/src/07-mocking-lib-api/index.test.ts
@@ -1,17 +1,52 @@
-// Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+import axios from 'axios';
+import { throttledGetDataFromApi } from './index';
+import runAllTimers = jest.runAllTimers;
+
+jest.mock('axios');
 
 describe('throttledGetDataFromApi', () => {
+  beforeAll(() => {
+    jest.useFakeTimers();
+  });
+
+  afterAll(() => {
+    jest.useRealTimers();
+  });
+
   test('should create instance with provided base url', async () => {
-    // Write your test here
+    const mockGet = jest.fn().mockResolvedValue({ data: 'mock data' });
+    const mockCreate = jest.fn(() => ({ get: mockGet }));
+    (axios.create as jest.Mock).mockImplementation(mockCreate);
+
+    throttledGetDataFromApi('/fake_url');
+    runAllTimers();
+
+    expect(mockCreate).toHaveBeenCalledWith({
+      baseURL: 'https://jsonplaceholder.typicode.com',
+    });
   });
 
   test('should perform request to correct provided url', async () => {
-    // Write your test here
+    const mockGet = jest.fn().mockResolvedValue({ data: 'mock data' });
+    const mockCreate = jest.fn(() => ({ get: mockGet }));
+    (axios.create as jest.Mock).mockImplementation(mockCreate);
+
+    throttledGetDataFromApi('/provided/url');
+    runAllTimers();
+
+    expect(mockGet).toHaveBeenCalledWith('/provided/url');
   });
 
-  test('should return response data', async () => {
-    // Write your test here
+  test('should return response data', (done) => {
+    const mockGet = jest.fn().mockResolvedValue({ data: 'mock data' });
+    const mockCreate = jest.fn(() => ({ get: mockGet }));
+
+    (axios.create as jest.Mock).mockImplementation(mockCreate);
+
+    throttledGetDataFromApi('/provided/url')?.then((data) => {
+      expect(data).toEqual('mock data');
+      done();
+    });
+    runAllTimers();
   });
 });
